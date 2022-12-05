fe4dfb4b9a5f0120b8faabb5224ab7785daf747a
GlatterAal <64667805+GlatterAal@users.noreply.github.com>
2022-12-06 00:25:29 +0100

    added fourth door

---
+++ Fourth/Fourth(a).py
@@ -0,0 +1,15 @@
+def main(file):
+    f=open(file,"r")
+    max=0
+    for line in f:
+        arr=line.split(",")
+        s1=int(arr[0].split("-")[0])
+        e1=int(arr[0].split("-")[1])
+        s2=int(arr[1].split("-")[0])
+        e2=int(arr[1].split("-")[1])
+        if((s1<=s2 and e1>=e2) or (s2<=s1 and e2>=e1)):
+            max+=1
+    return max
+
+print(main(r"C:\Users\pc\Desktop\Github\AdventOfCode2022\Fourth\input.txt"))
+        
